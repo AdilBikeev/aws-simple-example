8b9ed269527081d31ce225582fea6948d90678e8
AdilBikeev <bikeev1999@mail.ru>
2023-02-25 19:13:12 +0300

    Add simple publisher logic

---
+++ SqsPublisher/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+using SqsPublisher;
+
+const string CustomersQueueName = "customers";
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated()
+{
+    Id = Guid.NewGuid(),
+    Email = "fuf@mail.ru",
+    FullName = "F I O",
+    DateOfBirth = new DateTime(1, 1, 1),
+    GitHubUsername = "adcoder"
+};
+
+var queryUrlResponse = await sqsClient.GetQueueUrlAsync(CustomersQueueName);
+
+var message = sqsClient.SendMessageAsync(new SendMessageRequest()
+{
+    MessageBody = JsonSerializer.Serialize(customer),
+    QueueUrl = queryUrlResponse.QueueUrl
+});
