dc1c6d1905a435d6d593f3c6ee348ba53bf5027d
AdilBikeev <bikeev1999@mail.ru>
2023-03-25 21:57:19 +0300

    Add S3 playground

---
+++ S3Playground/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+const string bucketName = "fooawsbucket";
+const string key = "files/foo.txt";
+const string contentType = "text/plain";
+
+var client = new AmazonS3Client(RegionEndpoint.USEast1);
+
+// Put
+// await using var stream = new FileStream("./foo.txt", FileMode.Open, FileAccess.Read);
+// client = new AmazonS3Client(RegionEndpoint.USEast1);
+// await client.PutObjectAsync(new PutObjectRequest()
+// {
+//     BucketName = bucketName,
+//     Key = key,
+//     ContentType = contentType,
+//     InputStream = stream
+// });
+
+// Get
+var response = await client.GetObjectAsync(new GetObjectRequest()
+{
+    BucketName = bucketName,
+    Key = key,
+});
+using var memoryStream = new MemoryStream();
+response.ResponseStream.CopyTo(memoryStream);
+
+var txt = Encoding.Default.GetString(memoryStream.ToArray());
+Console.Write(txt);
