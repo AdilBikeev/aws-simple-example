81ac0d3e5fc122c9180147e4edba15c49b3c36b8
AdilBikeev <bikeev1999@mail.ru>
2023-04-02 15:53:03 +0300

    Add Sample using Amazon.SecretManager

---
+++ SecretManager/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+ *	Use this code snippet in your app.
+ *	If you need more information about configurations or implementing the sample code, visit the AWS docs:
+ *	https://aws.amazon.com/developer/language/net/getting-started
+ */
+
+using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+static async Task GetSecret()
+{
+    string secretName = "ApiKey";
+    string region = "us-east-1";
+
+    IAmazonSecretsManager client = new AmazonSecretsManagerClient(RegionEndpoint.GetBySystemName(region));
+
+    GetSecretValueRequest request = new GetSecretValueRequest
+    {
+        SecretId = secretName,
+        VersionStage = "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified.
+    };
+
+    try
+    {
+        var response = await client.GetSecretValueAsync(request);
+    }
+    catch (Exception e)
+    {
+        // For a list of the exceptions thrown, see
+        // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        throw e;
+    }
+}
+
+static async Task GetDescribeSecret()
+{
+    string secretName = "ApiKey";
+    string region = "us-east-1";
+
+    IAmazonSecretsManager client = new AmazonSecretsManagerClient(RegionEndpoint.GetBySystemName(region));
+
+    var request = new DescribeSecretRequest()
+    {
+        SecretId = secretName,
+    };
+
+    try
+    {
+       var response = await client.DescribeSecretAsync(request);
+    }
+    catch (Exception e)
+    {
+        // For a list of the exceptions thrown, see
+        // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        throw e;
+    }
+}
+
+await GetSecret();
+await GetDescribeSecret();
